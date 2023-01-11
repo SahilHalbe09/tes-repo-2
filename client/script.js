ebdc1da6ae5d32ab791e6595c3eee742062fd3cd
Sahil Halbe <sahilyhalbe@gmail.com>
2023-01-11 13:21:11 +0530

    minor changes

--- client/script.js
+++ client/script.js
@@ -94,10 +94,11 @@ function loader(element) {
 
 	loadInterval = setInterval(() => {
 		// Update the text content of the loading indicator
+		element.textContent = "Generating";
 		element.textContent += ".";
 
 		// If the loading indicator has reached three dots, reset it
-		if (element.textContent === "....") {
+		if (element.textContent === "Generating....") {
 			element.textContent = "";
 		}
 	}, 300);
